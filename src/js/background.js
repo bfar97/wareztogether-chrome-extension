07209850c045c07eea71dcf07b753ed93d3507e9
Bernardo Rodrigues <bernardinhofilipe@hotmail.com>
2015-01-10 17:36:34 +0000

    changes to backgroud.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,8 @@
 // emptied background.js file
 document.title = "WarezTogether";
+
+function myfunction() {
+	alert("hi!");
+}
+
+document.getElementById("container").addEventListener("click", myfunction);
