54c5c6da11fe5bfdfd702cf8bdaf0dd4b0aa4e31
Marcos Pereira <marcos97pereira@gmail.com>
2015-01-16 19:27:06 +0000

    Started experimenting with communication
    Also changed the little part about the alert on container click to
    jQuery instead of vanilla javascript.

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,4 +1,24 @@
-var socket = io('localhost:3000');
-
+/*var socket = io('localhost:3000');
 console.log('Emitting socket message');
-socket.emit('message', { my: 'data' });
+socket.emit('message', { my: 'data' });*/
+
+var warezTabId = false;
+
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+
+	if (request.from === 'wareztogether') {
+
+		if (request.action === 'init') {
+
+			warezTabId = sender.tab.id;
+			sendResponse({message: 'all ok!'});
+
+		} else {
+			console.log(request.message);
+
+			chrome.tabs.sendMessage(warezTabId, {from: 'wareztogether', message: 'clicked the container!'}, function(response) {
+				console.log(response.message);
+			});
+		}
+	}
+});
--- extension/js/content.js
+++ extension/js/content.js
@@ -1,9 +1,22 @@
-// emptied background.js file
 document.title = "WarezTogether";
 
-document.addEventListener("DOMContentLoaded", function () {
-	document.getElementById("container").addEventListener("click", function () {
-		alert("You've just clicked the video container!");
-	});
-})
+$(document).ready(function () {
 
+	chrome.runtime.sendMessage({from: 'wareztogether', action: 'init'}, function(response) {
+		//alert(response.message);
+	});
+
+	$('#play-box').on('click', function (event) {
+		//alert('You\'ve just clicked the video container!');
+
+		chrome.runtime.sendMessage({from: 'wareztogether', message: 'clicked some stuff!'});
+	});
+
+	chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+
+		if (request.from === 'wareztogether') {
+			//alert(request.message);
+		}
+	});
+
+});
