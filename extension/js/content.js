142b040cc076c29968b6fea12c49281c217927db
Bernardo Rodrigues <bernardinhofilipe@hotmail.com>
2015-01-16 19:44:26 +0000

    doent matter

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,9 +1,12 @@
 // emptied background.js file
 document.title = "WarezTogether";
 
-document.addEventListener("DOMContentLoaded", function () {
+// useless lines since we are using another player
+
+/* document.addEventListener("DOMContentLoaded", function () {
 	document.getElementById("container").addEventListener("click", function () {
 		alert("You've just clicked the video container!");
 	});
-})
+}) */
+
 
