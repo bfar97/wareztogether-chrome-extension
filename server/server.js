a328169fcaa1f1cdb033cd2a4b29929e8c89ce5a
Marcos Pereira <marcos97pereira@gmail.com>
2015-02-23 10:01:17 +0000

    Added bootstrap to popup.html & Default server msg

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-var server = require('http').createServer();
+var server = require('http').createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Bem vindo ao wareztogether :)');
+});
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
