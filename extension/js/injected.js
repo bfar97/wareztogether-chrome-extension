50219953c220f4b40aefa0868db90a829af1ca9d
Marcos Pereira <marcos97pereira@gmail.com>
2015-03-17 10:33:08 +0000

    Set up final event listeners for flowplayer API

--- extension/js/injected.js
+++ extension/js/injected.js
@@ -1,2 +1,42 @@
-// WarezTogether code that is injected into a <script> tag on the page so we can access the player globals
-window.postMessage({from: 'wareztogether', action: 'init', message: 'hello there :) ! :)'}, '*');
+/*
+ * WarezTogether code that is injected into a <script> tag on the iframe page so we can access the player globals (API)
+ */
+
+var player = $f();
+
+var firstStart = true;
+
+player.onStart(function () {
+
+	/* 
+	 * This event is ignored the first time it runs
+	 *
+	 * From the API docs: 
+	 * "With autoBuffering set to true it even fires when autoPlay is false,
+	 *  because the clip is paused at the first frame."
+	 *
+	 */
+
+	if (firstStart) {
+
+		firstStart = false;
+
+	} else {
+
+		window.postMessage({from: 'wareztogether', action: 'start'}, '*');
+	}
+});
+
+player.onBeforePause(function () {
+	window.postMessage({from: 'wareztogether', action: 'pause'}, '*');
+});
+
+player.onBeforeResume(function () {
+	window.postMessage({from: 'wareztogether', action: 'resume'}, '*');
+});
+
+//
+// React to events from server
+//
+
+
